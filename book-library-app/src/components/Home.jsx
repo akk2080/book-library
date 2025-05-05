ec3282354dd957d30f6d9abd7f52b2a89a50895f
AAKARSH <akk2080@gmail.com>
2025-05-05 14:28:27 +0530

    first commit

--- book-library-app/src/components/Home.jsx
+++ book-library-app/src/components/Home.jsx
@@ -8,8 +8,21 @@ function Home(){
     const {books,selectedBook,loading,error, errorMessage} = useSelector((state) => state.books);
     const dispatch = useDispatch();
     const [search, setSearch] = useState('');
+    const [currPage, setCurrPage] = useState(1);
 
+    const lastIndex = currPage*10;
+    const firstIndex = lastIndex - 10;
    console.log(books);
+
+  function handlePrev(){
+        if(currPage > 1)
+            setCurrPage(currPage-1);
+   }
+
+   function handleNext(){
+    if(currPage*10 < books.length)
+        setCurrPage(currPage+1);
+   }
 
     return (
         <>
@@ -22,8 +35,8 @@ function Home(){
                 {loading&&(<h1>loading...</h1>)}
                 {error&&(<h1>{errorMessage}</h1>)}
                 {!loading && books.length==0 && <h3>No data to show, Please try to enter something else on the search field</h3>}
-                <div className='mygrid'>
-                    {!loading && books && books.length !=0 && books.map((b, index) => {
+                
+                    {!loading && books && books.length !=0 && (<><div className='mygrid'> {books.slice(firstIndex, lastIndex).map((b, index) => {
                         return (
                             <div className='card'>
                                 <img src={`https://covers.openlibrary.org/b/olid/${b.cover_edition_key}-M.jpg`} alt='image not found'/>
@@ -32,9 +45,17 @@ function Home(){
                                 <button>view details</button>
                             </div>
                         )
-                    })}
+                    })}  
 
-                </div>
+                    
+                    
+                    </div>
+                <button onClick={handlePrev}>prev</button>
+                {currPage}
+                <button onClick={handleNext}>next</button>
+                </>)}
+
+               
                 
             </div>
         </>
